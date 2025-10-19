fefe7b9383d385dea418f5d06849e14719452d73
Mahaveer Soni <68422113+Mahaveer1141@users.noreply.github.com>
2025-10-19 13:38:37 +0530

    build: add layzgit to nvim

--- nvim/lua/plugins/common.lua
+++ nvim/lua/plugins/common.lua
@@ -245,4 +245,26 @@ return {
     'github/copilot.vim',
     version = '1.41.0',
   },
+
+  -- lazygit integrations with neovim
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 }
